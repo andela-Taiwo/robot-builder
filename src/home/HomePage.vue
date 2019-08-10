961ec7b5b51fc82ca80f3616670a3fdc249f0885
Taiwo Sokunbi <sokunbitaiwo82@gmail.com>
2019-08-10 14:42:29 +0100

    add home page add build page

---
+++ src/home/HomePage.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="home">
+  <div>
+    <img src="../assets/robot-home.png" class="robot" aria-hidden="true">
+  </div>
+  <div>
+    <a href=""> Getting Started </a> Build your own robot
+  </div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomePage',
+  props: {
+    msg: String,
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+  .robot {
+    height: 300px;
+  }
+  .home {
+    text-align: center;
+  }
+
+</style>
